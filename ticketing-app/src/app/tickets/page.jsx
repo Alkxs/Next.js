5ec3c53e3e33671f3e1a91243d1479b861f97d9f
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-08-22 19:30:38 +0200

    Ticket list implemented

--- ticketing-app/src/app/tickets/page.jsx
+++ ticketing-app/src/app/tickets/page.jsx
@@ -1,7 +1,18 @@
+import TicketList from '../components/TicketList'
+
 const Tickets = () => {
   return (
     <main>
-      <h2>Tickets</h2>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Open tickets</small>
+          </p>
+        </div>
+      </nav>
+
+      <TicketList />
     </main>
   )
 }
