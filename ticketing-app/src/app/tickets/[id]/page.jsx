9c9e2aadec3fbb43763c8251ced413e54722a29b
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-08-22 22:58:35 +0200

    ticket details pages completed

---
+++ ticketing-app/src/app/tickets/[id]/page.jsx
@@ -0,0 +1,27 @@
+async function getTicket(id) {
+  const res = await fetch('http://localhost:4000/tickets/' + id, {
+    next: {
+      revalidate: 60,
+    },
+  })
+
+  return res.json()
+}
+
+export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id)
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket details</h2>
+      </nav>
+      <div className='card'>
+        <h3>{ticket.title}</h3>
+        <small>created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+      </div>
+    </main>
+  )
+}
