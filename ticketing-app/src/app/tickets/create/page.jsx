cf2bc091d78a530713df72e5180a5da0560551ea
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-08-23 16:15:30 +0200

    Create Form page + POST requests implemented

---
+++ ticketing-app/src/app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import CreateForm from '../../components/CreateForm'
+
+export default function CreateTicket() {
+  return (
+    <main>
+      <h2 className='text-violet-800 text-center'>Add a New Ticket</h2>
+      <CreateForm />
+    </main>
+  )
+}
