73b112100712b3f5c318a7d920f20faef8f82cdc
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-08-25 12:03:57 +0200

    basic signup implementation completed

--- ticketing-app/src/app/(auth)/signup/page.jsx
+++ ticketing-app/src/app/(auth)/signup/page.jsx
@@ -1,16 +1,30 @@
 'use client'
 
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useState } from 'react'
 import AuthForm from '../AuthForm'
+import { useRouter } from 'next/navigation'
 
 export default function Signup() {
+  const router = useRouter()
+  const [error, setError] = useState('')
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault()
 
-    console.log(email, password, 'signup')
-
-    const login = {
+    const supabase = createClientComponentClient()
+    const { error } = await supabase.signUp({
       email,
       password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`,
+      },
+    })
+    if (error) {
+      setError(error.message)
+    }
+    if (!error) {
+      router.push('/verify')
     }
   }
 
@@ -18,6 +32,7 @@ export default function Signup() {
     <main>
       <h2 className='text-center'>Sign up</h2>
       <AuthForm handleSubmit={handleSubmit} />
+      {error && <div className='error'>{error}</div>}
     </main>
   )
 }
